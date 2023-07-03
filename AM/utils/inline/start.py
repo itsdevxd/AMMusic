aa1b34a77d60108427043cae1ca054901b61efda
Bhumi <137621182+Bhumikatosu@users.noreply.github.com>
2023-07-03 22:06:09 +0530

    Add files via upload

---
+++ AM/utils/inline/start.py
@@ -0,0 +1,56 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import config
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="➕ ᴀᴅᴅ ᴍᴇ ᴍᴏɪ ʙᴀʙʏ",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="🍂ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅ 🍂",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="sᴇᴛᴛɪɴɢs", callback_data="settings_helper"
+            ),
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="➕ ᴀᴅᴅ ᴍᴇ ᴍᴏɪ ʙᴀʙʏ",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="🍂ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅ 🍂", callback_data="settings_back_helper"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="💖 ᴄʜᴀᴛᴛɪɴɢ  💖", url=config.SUPPORT_GROUP
+            ),
+            InlineKeyboardButton(
+                text="🥀 ꜰᴏᴜɴᴅᴇʀ  🥀", user_id=OWNER
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="💘ɴᴇᴛᴡᴏʀᴋ💘", url=config.SUPPORT_CHANNEL
+            ),
+        ],
+     ]
+    return buttons
---
+++ AM/utils/logger.py
@@ -0,0 +1,39 @@
+from config import LOG, LOG_GROUP_ID, MUSIC_BOT_NAME
+from AM import app
+from AM.utils.database import is_on_off
+
+
+async def play_logs(message, streamtype):
+    if await is_on_off(LOG):
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
+        else:
+            chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+        logger_text = f"""
+**━━━━━━━━━━━━━━━**
+**🏼💞 {MUSIC_BOT_NAME} ᴩʟᴀʏ ʟᴏɢɢᴇʀ**
+
+**💬 ᴄʜᴀᴛ:** {message.chat.title} [`{message.chat.id}`]
+
+**👁‍🗨 ᴜsᴇʀ:** {message.from_user.mention}
+
+**🙋🏻‍♂️ ᴜsᴇʀɴᴀᴍᴇ:** @{message.from_user.username}
+
+**🆔 ɪᴅ:** `{message.from_user.id}`
+
+**🖇 ᴄʜᴀᴛ ʟɪɴᴋ:** {chatusername}
+
+**☯️ sᴇᴀʀᴄʜᴇᴅ ғᴏʀ:** `{message.text}`
+
+**sᴛʀᴇᴀᴍ ᴛʏᴩᴇ:** `{streamtype}`
+"""
+        if message.chat.id != LOG_GROUP_ID:
+            try:
+                await app.send_message(
+                    LOG_GROUP_ID,
+                    text=logger_text,
+                    disable_web_page_preview=True,
+                )
+            except:
+                pass
+        return
